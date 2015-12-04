251d1d935b71d2a3be82037d83e800c932d5e5bd
Mohan Shanmugarajah <s404853@emirates.com>
2015-12-04 16:04:37 +0400

    code  re-factored reflect the namespace properly

---
+++ Rome.RepositoryAccess/FlightRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rome.BusinessEntities;
+
+
+namespace Rome.RepositoryAccess
+{
+    public class FlightRepository  : IFlightRepository
+    {
+
+        #region Private Variables
+        private FlightEntity flightEntity;
+        #endregion
+
+        #region Constructors
+        public FlightRepository()
+        {
+            flightEntity = new FlightEntity();
+        }
+        #endregion
+
+        #region Public Methods
+        public FlightEntity getFlight()
+        {
+            System.Diagnostics.Debug.WriteLine("getFlight is being called from Repository layer ");
+            flightEntity.FlightNumber = "EK12345";
+            flightEntity.Model = "Boweying 777";
+            return flightEntity;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Rome.RepositoryAccess/IFlightRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rome.BusinessEntities;
+
+namespace Rome.RepositoryAccess
+{
+    public interface IFlightRepository
+    {
+        FlightEntity getFlight();
+    }
+}
